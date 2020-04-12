146b1cba277e2379baedd3944a1534585cb606cc
Ben Navetta <ben.navetta@gmail.com>
2020-04-11 17:23:04 -0700

    Flag for log levels

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::sync::Mutex;
 
 use anyhow::{Result, Context};
 use human_panic;
-use slog::{Logger, Drain, o, debug, error};
+use slog::{Logger, Drain, Level, LevelFilter, o, debug, error};
 use slog_term;
 use structopt::StructOpt;
 use tabwriter;
@@ -30,6 +30,11 @@ struct Args {
     /// Path to the Rustic configuration file
     #[structopt(short = "c", long = "config", env = "RUSTIC_CONFIG", parse(from_os_str))]
     config_file: PathBuf,
+
+    /// Adjust the verbosity of log output. By default, only print errors and warnings. Pass `-v` for informational messages or
+    /// `-vv` for debug messages.
+    #[structopt(short = "v", long = "verbose", parse(from_occurrences))]
+    verbose: u8,
 
     #[structopt(subcommand)]
     command: Command
@@ -101,6 +106,7 @@ fn list_profiles(config: &Configuration) -> Result<()> {
 fn run(args: Args, logger: &Logger) -> Result<()> {
     let config = load_config(logger, &args.config_file)?;
 
+    // TODO: pass verbosity flag along to restic
     match args.command {
         Command::Backup { profile } => {
             let restic = Restic::for_profile(&config, logger, profile)?;
@@ -130,13 +136,23 @@ fn run(args: Args, logger: &Logger) -> Result<()> {
 fn main(args: Args) {
     human_panic::setup_panic!();
 
+    // The clap-verbosity-flag crate adds a `-q` flag for setting the log level below the default. However, for a backup program, it seems
+    // risky to allow hiding errors/warnings
+    let slog_level = match args.verbose {
+        0 => Level::Warning,
+        1 => Level::Info,
+        2 => Level::Debug,
+        _ => Level::Trace,
+    };
+
     let decorator = slog_term::TermDecorator::new().build();
     let term_drain = slog_term::FullFormat::new(decorator).use_local_timestamp().build().fuse();
     // Despite the slog docs, we're using a Mutex for the thread-safe drain rather than slog_async. Since this is a single-threaded program, there's
     // probably more overhead adding a thread for logging than letting the main thread use a mutex uncontested. This also means we can use the logger
     // below without having to worry about flushing it before calling std::process::exit
-    let drain = Mutex::new(term_drain).fuse();
-    let root = Logger::root(drain, o!("rustic_version" => env!("CARGO_PKG_VERSION")));
+    let drain = Mutex::new(term_drain);
+    let filtered = LevelFilter::new(drain, slog_level).fuse();
+    let root = Logger::root(filtered, o!("rustic_version" => env!("CARGO_PKG_VERSION")));
 
     if let Err(err) = run(args, &root) {
         error!(root, "Fatal error: {:?}", err);
